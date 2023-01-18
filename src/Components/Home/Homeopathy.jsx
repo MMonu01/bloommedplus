097fce638e05b6d0b51585a4e99402a2e190d810
rajaprhub <98698646+rajaprhub@users.noreply.github.com>
2023-01-18 21:33:37 +0530

    homepage

---
+++ src/Components/Home/Homeopathy.jsx
@@ -0,0 +1,69 @@
+
+
+import axios from "axios"
+import React, { useState } from "react"
+import styles from '../../Styles/Homeopathy.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronRight,faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+const GetData = ()=>{
+    return axios({
+        method:"get",
+        baseURL:"http://localhost:1010/Landing_page"
+    })
+}
+export const Homeopathy = ()  =>{
+const [data,setData]=useState([])
+const [loading,setLoading] = useState(false)
+React.useEffect(()=>{
+InGetData()
+},[])
+console.log(data,"oneojos")
+const InGetData = ()=>{
+    setLoading(true)
+    GetData()
+    .then((res)=>{
+        setData(()=>res.data[7].homeopathy)
+        setLoading(false)
+    })
+    .catch((err)=>{
+        console.log(err)
+        setLoading(false)
+    })
+}
+let box = document.querySelector(".one")
+console.log("box",box)
+const buttonPressRev = ()=>{
+let width =  box.clientWidth
+box.scrollLeft -= width
+}
+const buttonPressNext =()=>{
+    let width =  box.clientWidth
+    box.scrollLeft += width
+}
+return(
+    <div>
+        <h3 style={{background:"whitesmoke" ,textAlign:"left", width:"95%",margin: "auto",
+        color:"black" }} > Homeotpathy </h3>
+    <div className={styles.box}>
+        <div onClick={buttonPressRev}><FontAwesomeIcon  className={styles.icon} icon={faChevronLeft}/></div>
+    <div className={styles.container} style={{width:"100%"}}>
+        <div className="one" style={{scrollBehavior:"smooth",background:"white"}}>
+        {
+            loading===false ? data.map((el)=>(
+                <div key={el.id}>
+                    <img src={el.image} alt={el.image}/>
+                    <p>{el.name}</p>
+                    </div>
+            )):null
+        }
+        </div>
+    </div>
+    <div onClick={buttonPressNext}><FontAwesomeIcon className={styles.icon} icon={faChevronRight}/></div>
+    </div>
+    </div>
+)
+}
+
+
+
+
---
+++ src/Components/Home/PersonalCare.jsx
@@ -0,0 +1,67 @@
+
+
+import axios from "axios"
+import React, { useState } from "react"
+import styles from '../../Styles/PersonalCare.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faChevronRight,faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+const GetData = ()=>{
+    return axios({
+        method:"get",
+        baseURL:"http://localhost:1010/Landing_page"
+    })
+}
+export const PersonalCare = ()  =>{
+const [data,setData]=useState([])
+const [loading,setLoading] = useState(false)
+React.useEffect(()=>{
+InGetData()
+},[])
+console.log(data,"oneojos")
+const InGetData = ()=>{
+    setLoading(true)
+    GetData()
+    .then((res)=>{
+        setData(()=>res.data[4].Personal_care)
+        setLoading(false)
+    })
+    .catch((err)=>{
+        console.log(err)
+        setLoading(false)
+    })
+}
+let box = document.querySelector(".one")
+console.log("box",box)
+const buttonPressRev = ()=>{
+let width =  box.clientWidth
+box.scrollLeft -= width
+}
+const buttonPressNext =()=>{
+    let width =  box.clientWidth
+    box.scrollLeft += width
+}
+return(
+    <>
+    <div className={styles.box}>
+        <div onClick={buttonPressRev}><FontAwesomeIcon  className={styles.icon} icon={faChevronLeft}/></div>
+    <div className={styles.container} style={{width:"100%"}}>
+        <div className="one" style={{scrollBehavior:"smooth",background:"white"}}>
+        {
+            loading===false ? data.map((el)=>(
+                <div key={el.id}>
+                    <img src={el.image} alt={el.image}/>
+                    <p>{el.name}</p>
+                    </div>
+            )):null
+        }
+        </div>
+    </div>
+    <div onClick={buttonPressNext}><FontAwesomeIcon className={styles.icon} icon={faChevronRight}/></div>
+    </div>
+    </>
+)
+}
+
+
+
+
