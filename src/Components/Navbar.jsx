8d80d4bcf6cc44be0d18afeb24bbc6dda0cfb821
Shibangi Das <shibangid319@gmail.com>
2023-01-20 23:46:58 +0530

    added LoginSignup

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,185 +1,318 @@
-import { Link,Navigate,useSearchParams } from "react-router-dom"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Link, Navigate, useSearchParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { faCartShopping, faLocationDot,faLocationCrosshairs,faMagnifyingGlass,faChevronDown,faBars } from '@fortawesome/free-solid-svg-icons'
-import styles from '../Styles/Navbar.module.css'
-import { Input,InputRightElement,Button,InputGroup } from '@chakra-ui/react'
-import Sidebar from "./Sidebar"
-import React,{ useState,useContext } from "react"
-import { CartContext } from "../Contexts/CartContext"
-import { SearchContext } from "../Contexts/SearchContext"
-export const Navbar = ()=>{
+import {
+  faCartShopping,
+  faLocationDot,
+  faLocationCrosshairs,
+  faMagnifyingGlass,
+  faChevronDown,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
+import styles from "../Styles/Navbar.module.css";
+import { Input, InputRightElement, Button, InputGroup } from "@chakra-ui/react";
+import Sidebar from "./Sidebar";
+import React, { useState, useContext } from "react";
+import { CartContext } from "../Contexts/CartContext";
+import { SearchContext } from "../Contexts/SearchContext";
+import Signup from "../Pages/Signup";
+import Login from "../Pages/Login";
 
-  
-  const [searchParams,setSearchParams] = useSearchParams()
-  const All = searchParams.get("sort")
+export const Navbar = () => {
+  const [show, setShow] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleLoginClose = () => setShowLogin(false);
+  const handleLoginShow = () => setShowLogin(true);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const All = searchParams.get("sort");
   // const getSearched = searchParams.get("q")
-  const{search,setSearch} = useContext(SearchContext)
+  const { search, setSearch } = useContext(SearchContext);
   // const [search,setSearch] = useState(getSearched || "")
-const {userCart} = useContext(CartContext)
-const [cartItem,setCartItem] = useState(0)
+  const { userCart } = useContext(CartContext);
+  const [cartItem, setCartItem] = useState(0);
 
+  React.useEffect(() => {
+    setCartItem(userCart.cart.length);
+  }, [userCart]);
 
-React.useEffect(()=>{
-setCartItem(userCart.cart.length)
-},[userCart])
+  React.useEffect(() => {
+    setSearchParams({ sort: All, q: search });
+  }, [search]);
 
+  const HandleSearchKey = (e) => {
+    if (e.keyCode == 13) {
+      // return <Navigate to='/products'/>
+      // console.log("this is enter button")
+    }
+    // console.log(e)
+  };
 
-  React.useEffect(()=>{
-    setSearchParams({sort:All,q:search})
-},[search])
+  const HandleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-  const HandleSearchKey = (e)=>{
-    if(e.keyCode==13){
-  // return <Navigate to='/products'/>
-  // console.log("this is enter button")
-}
-// console.log(e)
-  }
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.laptop}>
+          <div className={styles.top}>
+            <div>
+              <Link style={{ background: "white" }} to="/">
+                <img
+                  className={styles.logo}
+                  src="../../Images/bloommedplus.png"
+                  alt="logo"
+                />
+              </Link>
+            </div>
+            <div className={styles.medicine}>MEDICINES</div>
+            <div>
+              <span className={styles.head}>LAB TESTS </span>{" "}
+              <span style={{ fontSize: "12px" }} className={styles.safe}>
+                SAFE
+              </span>
+            </div>
+            <div className={styles.head}>CONSULT DOCTORS</div>
+            <div className={styles.head}>COVID-19</div>
+            <div className={styles.head}>AYURVEDA</div>
+            <div>
+              <span className={styles.head}>CARE PLAN </span>{" "}
+              <span className={styles.safe}>SAVE MORE</span>
+            </div>
+            <div>
+              <span className={styles.login} onClick={handleLoginShow}>
+                Login
+              </span>{" "}
+              |{" "}
+              <span className={styles.signup} onClick={handleShow}>
+                Signup
+              </span>
+            </div>
+            <div>Offers</div>
+            <div style={{ width: "35px", cursor: "pointer" }}>
+              <Link
+                to="/cart"
+                style={{ background: "white", textDecoration: "none" }}
+              >
+                <FontAwesomeIcon
+                  style={{ color: "black", background: "white" }}
+                  icon={faCartShopping}
+                />
+                <sup style={{ background: "white" }}>{cartItem}</sup>
+              </Link>
+            </div>
+            <div>Need Help?</div>
+          </div>
 
-  const HandleSearchChange = (e)=>{
-setSearch(e.target.value)
+          <hr className={styles.hr} />
 
-  }
+          <div className={styles.middle}>
+            <div className={styles.left}>
+              <div className={styles.delhi}>
+                <div>
+                  <FontAwesomeIcon
+                    icon={faLocationDot}
+                    className={styles.dot}
+                  />{" "}
+                  <span style={{ fontWeight: "bold", marginLeft: "5px" }}>
+                    {" "}
+                    New Delhi
+                  </span>
+                </div>
+                <div>
+                  <FontAwesomeIcon
+                    icon={faLocationCrosshairs}
+                    className={styles.cross}
+                  />
+                </div>
+              </div>
 
+              <div className={styles.Search}>
+                <input
+                  className={styles.input}
+                  onKeyUp={HandleSearchKey}
+                  onChange={HandleSearchChange}
+                  value={search}
+                  type="text"
+                  placeholder="Search for Medicines and Health Products"
+                />
+                <Link to="/products/skincare">
+                  <FontAwesomeIcon
+                    className={styles.glass}
+                    icon={faMagnifyingGlass}
+                  />
+                </Link>
+              </div>
+            </div>
 
-    return (
-     <nav className={styles.navbar}>
-      <div className={styles.laptop}>
+            <div className={styles.right}>
+              <div className={styles.quick}>
+                QUICK BUY! Get 25% off on
+                <div>
+                  <p style={{ float: "right", background: "white" }}>
+                    medicines*
+                  </p>
+                </div>
+              </div>
+              <div style={{ background: "white" }}>
+                <button className={styles.order}>Quick order</button>
+              </div>
+            </div>
+          </div>
 
+          <hr className={styles.hr} />
+          {/* ------------------------------------------------------------------------ */}
 
-<div className={styles.top}>
+          <div className={styles.bottom}>
+            <div>
+              <span>Health Care Resource</span>{" "}
+              <FontAwesomeIcon className={styles.down} icon={faChevronDown} />
+            </div>
+            <div>
+              <span>Vitamins & Nutritions</span>{" "}
+              <FontAwesomeIcon className={styles.down} icon={faChevronDown} />
+            </div>
+            <div>
+              <span>diabetes</span>{" "}
+              <FontAwesomeIcon className={styles.down} icon={faChevronDown} />
+            </div>
+            <div>
+              <span>Healthcare Devices</span>{" "}
+              <FontAwesomeIcon className={styles.down} icon={faChevronDown} />
+            </div>
+            <div>
+              <span>Personal Care</span>{" "}
+              <FontAwesomeIcon className={styles.down} icon={faChevronDown} />
+            </div>
+            <div>
+              <span>Health Conditions</span>{" "}
+              <FontAwesomeIcon className={styles.down} icon={faChevronDown} />
+            </div>
+            <div>
+              <span>Ayurveda Products</span>{" "}
+              <FontAwesomeIcon className={styles.down} icon={faChevronDown} />
+            </div>
+            <div>
+              <span>Homeopathy</span>{" "}
+              <FontAwesomeIcon className={styles.down} icon={faChevronDown} />
+            </div>
+            <div>
+              <span>Featured</span>{" "}
+              <FontAwesomeIcon className={styles.down} icon={faChevronDown} />
+            </div>
+            <div>
+              <span>Covid Essentials</span>{" "}
+              <FontAwesomeIcon className={styles.down} icon={faChevronDown} />
+            </div>
+          </div>
+        </div>
 
-<div><Link style={{background:"white"}} to='/'><img className={styles.logo} src="../../Images/bloommedplus.png" alt="logo"/></Link></div>
-<div className={styles.medicine}>MEDICINES</div>
-<div><span className={styles.head}>LAB TESTS  </span> <span style={{fontSize:"12px"}} className={styles.safe}>SAFE</span></div>
-<div className={styles.head}>CONSULT DOCTORS</div>
-<div className={styles.head}>COVID-19</div>
-<div className={styles.head}>AYURVEDA</div>
-<div><span className={styles.head}>CARE PLAN  </span> <span className={styles.safe}>SAVE MORE</span></div>
-<div><span className={styles.login}>Login</span> | <span className={styles.signup}>Signup</span></div>
-<div>Offers</div>
-<div style={{width:"35px",cursor:"pointer"}}><Link to='/cart' style={{background:'white',textDecoration:"none"}}>
-  <FontAwesomeIcon style={{color:"black",background:"white"}} icon={faCartShopping}/><sup style={{background:"white"}}>{cartItem}</sup></Link></div>
-<div>Need Help?</div>
-</div>
+        {/* ---------------------------------------------------------------------------------------- */}
 
+        <div className={styles.newNav}>
+          <div className={styles.newTop}>
+            <div>
+              <Sidebar />
+              <div>
+                <Link style={{ background: "white" }} to="/">
+                  <img src="../../Images/bloommedplus.png" alt="logo" />
+                </Link>
+              </div>
+            </div>
 
+            <div style={{ background: "white" }}>
+              <Link
+                to="/cart"
+                style={{ background: "white", textDecoration: "none" }}
+              >
+                <FontAwesomeIcon
+                  icon={faCartShopping}
+                  style={{
+                    background: "white",
+                    textDecoration: "none",
+                    color: "black",
+                  }}
+                />
+                <sup style={{ background: "white" }}>{cartItem}</sup>
+              </Link>
+            </div>
+          </div>
+          <hr />
+          <div className={styles.newMiddle}>
+            <div>
+              <FontAwesomeIcon icon={faLocationDot} className={styles.dot} />{" "}
+              <span style={{ fontWeight: "bold", marginLeft: "5px" }}>
+                {" "}
+                New Delhi
+              </span>{" "}
+              <FontAwesomeIcon
+                className={styles.newdown}
+                icon={faChevronDown}
+              />
+            </div>
+          </div>
 
-
-<hr className={styles.hr}/> 
-
-
-
- <div className={styles.middle}>
-
-<div className={styles.left}>
-
-<div className={styles.delhi}>
-<div><FontAwesomeIcon icon={faLocationDot} className={styles.dot}/> <span style={{fontWeight:"bold",marginLeft:"5px"}}> New Delhi</span></div>
-<div><FontAwesomeIcon icon={faLocationCrosshairs} className={styles.cross}/></div> 
-</div>
-
-<div className={styles.Search}>
-  <input className={styles.input} onKeyUp={HandleSearchKey} onChange={HandleSearchChange} value={search} type="text" placeholder="Search for Medicines and Health Products"/>
-  <Link to='/products/skincare' >
-  <FontAwesomeIcon className={styles.glass} icon={faMagnifyingGlass}/>
-  </Link>
-</div> 
-
-
-</div>
-
-
-
-<div className={styles.right}>
-<div className={styles.quick}>
-QUICK BUY! Get 25% off on 
-<div >
-<p style={{float:"right",background:'white'}}>medicines*</p>
-</div>
-</div>
-<div style={{background:"white"}}>
-  <button className={styles.order}>Quick order</button>
-</div>
-</div>
-
-
-</div> 
-
-
-
- <hr className={styles.hr}/> 
-{/* ------------------------------------------------------------------------ */}
-
-
-
-
-<div className={styles.bottom}>
-
-<div><span>Health Care Resource</span> <FontAwesomeIcon className={styles.down} icon={faChevronDown}/></div>
-<div><span>Vitamins & Nutritions</span> <FontAwesomeIcon className={styles.down} icon={faChevronDown}/></div>
-<div><span>diabetes</span> <FontAwesomeIcon className={styles.down} icon={faChevronDown}/></div>
-<div><span>Healthcare Devices</span> <FontAwesomeIcon className={styles.down} icon={faChevronDown}/></div>
-<div><span>Personal Care</span> <FontAwesomeIcon className={styles.down} icon={faChevronDown}/></div>
-<div><span>Health Conditions</span> <FontAwesomeIcon className={styles.down} icon={faChevronDown}/></div>
-<div><span>Ayurveda Products</span> <FontAwesomeIcon className={styles.down} icon={faChevronDown}/></div>
-<div><span>Homeopathy</span> <FontAwesomeIcon className={styles.down} icon={faChevronDown}/></div>
-<div><span>Featured</span> <FontAwesomeIcon className={styles.down} icon={faChevronDown}/></div>
-<div><span>Covid Essentials</span> <FontAwesomeIcon className={styles.down} icon={faChevronDown}/></div>
-
-</div>
- 
-      </div>
-
-{/* ---------------------------------------------------------------------------------------- */}
-
-<div className={styles.newNav}>
-
-
-<div className={styles.newTop}>
-
-<div>  
-<Sidebar/>
-<div><Link style={{background:"white"}} to='/'><img  src="../../Images/bloommedplus.png" alt="logo"/></Link></div>
-</div>
-
-<div style={{background:"white"}}>
-<Link to='/cart' style={{background:'white',textDecoration:"none"}}>
-<FontAwesomeIcon icon={faCartShopping} style={{background:"white",textDecoration:"none",color:"black"}} />
-<sup style={{background:"white"}}>{cartItem}</sup>
-</Link>
-</div>
-</div>
-<hr />
-<div className={styles.newMiddle}>
-<div><FontAwesomeIcon icon={faLocationDot} className={styles.dot}/> <span style={{fontWeight:"bold",marginLeft:"5px"}}> New Delhi</span> <FontAwesomeIcon className={styles.newdown}  icon={faChevronDown}/>
-</div>
-</div>
-
-<div className={styles.newbottom}>
-<div className={styles.newSearch}>
-<InputGroup size='md' style={{background:"white",border:"1px solid rgb(168, 168, 168)",borderRadius:"5px"}}>
-      <Input
-      style={{background:"white",padding:"10px 20px",marginLeft:"5px",outline:"none",border:"none",}}
-        pr='4.5rem'
-        type="text"
-        onKeyUp={HandleSearchKey} onChange={HandleSearchChange} value={search}
-        placeholder='Search for Medicines and Health Products'
+          <div className={styles.newbottom}>
+            <div className={styles.newSearch}>
+              <InputGroup
+                size="md"
+                style={{
+                  background: "white",
+                  border: "1px solid rgb(168, 168, 168)",
+                  borderRadius: "5px",
+                }}
+              >
+                <Input
+                  style={{
+                    background: "white",
+                    padding: "10px 20px",
+                    marginLeft: "5px",
+                    outline: "none",
+                    border: "none",
+                  }}
+                  pr="4.5rem"
+                  type="text"
+                  onKeyUp={HandleSearchKey}
+                  onChange={HandleSearchChange}
+                  value={search}
+                  placeholder="Search for Medicines and Health Products"
+                />
+                <InputRightElement
+                  style={{
+                    background: "white",
+                    width: "20px",
+                    marginRight: "15px",
+                  }}
+                >
+                  <Link to="/products/skincare" style={{ background: "white" }}>
+                    <FontAwesomeIcon
+                      style={{
+                        color: "black",
+                        background: "white",
+                        marginRight: "5px",
+                        outline: "none",
+                        paddingLeft: "5px",
+                        cursor: "pointer",
+                        marginTop: "9px",
+                      }}
+                      icon={faMagnifyingGlass}
+                    />
+                  </Link>
+                </InputRightElement>
+              </InputGroup>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Signup handleShow={handleShow} handleClose={handleClose} show={show} />
+      <Login
+        handleLoginShow={handleLoginShow}
+        handleLoginClose={handleLoginClose}
+        showLogin={showLogin}
       />
-      <InputRightElement   style={{background:"white",width:"20px",marginRight:"15px"}}>
-       <Link to="/products/skincare" style={{background:"white"}}>
-  <FontAwesomeIcon style={{color:"black",background:"white",marginRight:"5px",outline:"none",paddingLeft:"5px",cursor:"pointer",marginTop:"9px"}}  icon={faMagnifyingGlass}/>
-       </Link>
-
-   
-      </InputRightElement>
-    </InputGroup>
-</div>
-</div>
-
-</div>
-
-     </nav>
-    )
-}
+    </>
+  );
+};
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,10 +1,92 @@
-const Login = ()=>{
-    return (
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import modalStyle from "../Styles/SignupModal/modal.module.css";
+import FormGroup from "react-bootstrap/esm/FormGroup";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { Flex, Button } from "@chakra-ui/react";
+
+const Login = ({ showLogin, handleLoginShow, handleLoginClose }) => {
+  const [showLoginPassword, setShowLoginPassword] = useState(false);
+
+  return (
+    <Modal
+      show={showLogin}
+      onHide={handleLoginClose}
+      className={modalStyle.mainMod}
+    >
+      <Flex>
         <div>
-            Login
+          <img
+            className={modalStyle.pharmaLogin}
+            alt=""
+            src="../../Images/pharma.jpg"
+          />
         </div>
-    )
-}
+        <div className={modalStyle.leftForm}>
+          <Modal.Header className={modalStyle.cls}>
+            <Modal.Title className={modalStyle.title}>Login</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group>
+                <Form.Control
+                  className={modalStyle.mail}
+                  type="email"
+                  placeholder="Enter your email"
+                  autoFocus
+                />
+              </Form.Group>
 
+              <Button
+                className={modalStyle.btn}
+                variant={"ghost"}
+                onClick={() =>
+                  setShowLoginPassword(
+                    (showLoginPassword) => !showLoginPassword
+                  )
+                }
+              >
+                {showLoginPassword ? (
+                  <ViewIcon
+                    backgroundColor={"#ffffff"}
+                    color={"rgb(81, 75, 75);"}
+                  />
+                ) : (
+                  <ViewOffIcon
+                    backgroundColor={"#ffffff"}
+                    color={"rgb(81, 75, 75);"}
+                  />
+                )}
+              </Button>
+              <Form.Group>
+                <Form.Control
+                  className={modalStyle.pwd}
+                  type={showLoginPassword ? "text" : "password"}
+                  placeholder="Enter your password"
+                  autoFocus
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={handleLoginClose} className={modalStyle.create}>
+              Create an account
+            </Button>
+          </Modal.Footer>
+          <Flex gap="5%" alignItems={"center"} justify={"center"}>
+            <Button onClick={handleLoginClose} className={modalStyle.close}>
+              Close
+            </Button>
 
-export default Login
+            <Button onClick={handleLoginClose} className={modalStyle.login}>
+              Signup
+            </Button>
+          </Flex>
+        </div>
+      </Flex>
+    </Modal>
+  );
+};
+
+export default Login;
