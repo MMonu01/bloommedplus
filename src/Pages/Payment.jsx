66187f7d7ffbc148325fac17b3fa472cbf6f5dd3
Shibangi Das <shibangid319@gmail.com>
2023-01-21 22:12:05 +0530

    added address.jsx

---
+++ src/Pages/Payment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "../Components/Navbar";
+
+const Payment = () => {
+  return (
+    <>
+      <Navbar />
+      <div></div>
+    </>
+  );
+};
+
+export default Payment;
